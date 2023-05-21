d9529cede3c6e76c84b571847590b10729bb9f46
rybaaa <bolshayaryba22@gmail.com>
2023-05-22 00:59:17 +0200

    feat: checking last digit of card number using Luhn’s algorithm

--- src/repositories/validation-repository.ts
+++ src/repositories/validation-repository.ts
@@ -1,4 +1,4 @@
-const errors:ErrorsType = {
+const errors: ErrorsType = {
     cardNumberError: '',
     dateError: '',
     cvvError: ''
@@ -9,17 +9,44 @@ const hasNonDigitSymbols = (str: string) => {
     return pattern.test(str);
 }
 
+const checkLastDigit = (cardNumber: string) => {
+    const digits = cardNumber.split('').map(Number);
+    if (cardNumber.length % 2 === 0) {
+        for (let i = digits.length - 2; i >= 0; i -= 2) {
+            digits[i] *= 2;
+            if (digits[i] > 9) {
+                digits[i] -= 9;
+            }
+        }
+    } else {
+        for (let i = digits.length - 1; i >= 0; i -= 2) {
+            digits[i] *= 2;
+            if (digits[i] > 9) {
+                digits[i] -= 9;
+            }
+        }
+    }
+    const sum = digits.reduce((acc, digit) => acc + digit, 0);
+    const sumWithoutLastDigit = sum - digits[digits.length - 1]
+    return (sumWithoutLastDigit + digits[digits.length - 1]) % 10 === 0;
+}
+
+
 const isCardNumberValid = (cardNumber: string) => {
     if (cardNumber.length < 16 || cardNumber.length > 19 || hasNonDigitSymbols(cardNumber)) {
         errors.cardNumberError = 'Card number should be between 16 and 19 digits long'
+    } else if (!checkLastDigit(cardNumber)) {
+        errors.cardNumberError = 'Invalid card number'
     } else {
         errors.cardNumberError = ''
     }
 }
+
 const isDateValid = (month: string, year: string) => {
     const date = new Date();
     const monthNow = date.getMonth() + 2;
     const yearNow = date.getFullYear();
+
     if (hasNonDigitSymbols(month) || hasNonDigitSymbols(year)) {
         errors.dateError = 'Wrong date, digits only'
     } else if (month.length !== 2 || year.length !== 4) {
@@ -52,7 +79,7 @@ const isCvvValid = (cardNumber: string, cvv: string) => {
     }
 }
 export const validationRepository = {
-    async validateData(cardNumber: string, month: string, year: string, cvv: string):Promise<ErrorsType> {
+    async validateData(cardNumber: string, month: string, year: string, cvv: string): Promise<ErrorsType> {
         isCardNumberValid(cardNumber)
         isDateValid(month, year)
         isCvvValid(cardNumber, cvv)
